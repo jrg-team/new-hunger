edd3fba00634a6bdda3c3d66068486dadae1c89f
Zegendary <zxw1992513@qq.com>
2018-08-16 21:47:04 +0800

    课程评价部分完成

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ let studySwiper = {
     clickable: true
   }
 }
+let commentSwiperPhone = {
+  autoplay: {
+    autoplay: true,
+    disableOnInteraction: false
+  }
+}
 new Swiper ('.study-swiper-container', studySwiper)
+
 
 $('.curriculum-wrapper').on('click','li',function () {
   $(this).toggleClass('active')
@@ -30,3 +37,38 @@ $('.connection-pc').on('click','li',function () {
 let wechat_qrs = $('.wechat_qr')
 let index = Math.floor(wechat_qrs.length * Math.random())
 wechat_qrs.eq(index).show()
+
+if($(document).width() > 800){
+  new Swiper ('.comment-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+      autoplay: true,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.comment-swiper-pagination',
+      clickable: true
+    }
+  })
+} else if($(document).width() < 800 && $(document).width() > 500){
+  new Swiper ('.comment-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay: {
+      autoplay: true,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.comment-swiper-pagination',
+      clickable: true
+    }
+  })
+} else{
+  new Swiper ('.comment-container', {
+    autoplay: {
+      autoplay: true,
+      disableOnInteraction: false
+    }
+  })
+}
