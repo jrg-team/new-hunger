ef816a24c74846ea56755f39d886c682ba4acc58
chenwenhao <cwenhao88888@gmail.com>
2018-08-17 10:29:30 +0800

    完成动画

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import './js/import-jquery'
 import Swiper from 'swiper'
 
+$(document).ready(function () {
+  $('#banner-bg').animate({
+    top:"-400px",
+    opacity: 0.2
+  }, 520,function(){
+    $('#banner-bg').animate({ opacity: 1 },400)
+  });
+
+  $('#engineer').animate({
+    left:"0px"
+  },800)
+  $('#engineer + p').animate({
+    opacity: 1
+  },2500)
+  $('.book-button').animate({
+    left:"0px"
+  },900)
+  $('#book-details').animate({
+    right:"0px",
+    opacity: 0.5
+  },900)
+})
 
 let studySwiper = {
   spaceBetween: 13,
@@ -21,4 +43,19 @@ $('.curriculum-wrapper').on('click','li',function () {
 
 $('.qanda-wrapper').on('click','li',function () {
   $(this).toggleClass('active')
+})
+
+$('.book-button > span').on('click',function(){
+  $(this).toggleClass('active')
+})
+
+$('.banner-wrapper .aside > .loadMore').on('click',function(){
+  $(this).find('.load-wrap').toggleClass('active')
+})
+
+$('section.banner-wrapper > .content-wrapper > header').on('click','li',function(){
+  $(this).parent('ul').find('li').each((index,node)=>{
+    $(node).removeClass('active')
+  })
+  $(this).addClass('active')
 })
